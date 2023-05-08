3ee271eae6b32613b1fae4a11e695bfe57766df2
Flor Paramidani <florparamidani@gmail.com>
2023-05-07 23:41:01 -0300

    VII.MAQUETADO

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +9,27 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+router.post('/', async (req, res, next) )
+console.log (req.body)
+
+var name = req.body.name;
+var email = req.body.email;
+var message = req.body.message;
+
+var obj = {
+to: 'florparamidani@gamail.com',
+subject: 'Contacto desde la web',
+html: name + email + message
+}
+
+var transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  }
+})
+
+var info = await transporter.sendMail(obj);
+
