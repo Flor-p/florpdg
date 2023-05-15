c1086b148200ddb0bf7bcdac6d1a5daecb8f9a49
Flor Paramidani <florparamidani@gmail.com>
2023-05-14 22:59:37 -0300

    PROYECTO UTN FINAL_FLOR PARAMIDANI 2023

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-require('dotenv').config();
-
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
