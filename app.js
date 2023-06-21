7c3a9a7ca792066e72b8d74b94be75abf7ffda0b
Flor Paramidani <florparamidani@gmail.com>
2023-06-21 20:52:54 -0300

    Flor-Paramidani-Proyecto-Final-UTN_JUN-2023

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var logger = require('morgan');
 
 
 require('dotenv').config();
+var pool = require('./models/bd');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/admin/login')
 
 var app = express();
 // view engine setup
@@ -21,9 +23,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/admin/login', loginRouter);
+
+
+//select
+pool.query("select * from empleados").then(function(resultados){
+  console.log(resultados);
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
